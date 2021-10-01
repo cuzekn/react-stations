c2b85b3fdc4b8d88306f2c67514c067ed889d3a8
cuzekn <86640029+cuzekn@users.noreply.github.com>
2021-10-01 21:44:28 +0900

    image join

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ export const App = () => {
   return (
     <header>
       Dog Photo
+      <p>犬の画像を表示するサイトです。</p>
+      <img src="https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_3534.jpg" />
     </header>
   )
 }
