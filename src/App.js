5e1f3b05b7fdbcd143b8bc8aed564713de0e3af3
cuzekn <86640029+cuzekn@users.noreply.github.com>
2021-10-12 12:46:25 +0900

    1

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_3534.jpg");
-  fetch("https://dog.ceo/api/breeds/image/random")
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_3534.jpg',)
+
+  const handleClick = () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
     .then(res => res.json())
-    .then((result) => {
-      this.useState()
+    .then(result => {
+      const dog = result.message
+      setDogUrl(dog)
     })
+    .catch(error => {console.log(error)})
+  }
   return (
     <header>
       Dog Photo
       <p>犬の画像を表示するサイトです。</p>
       <img src={dogUrl} />
-      <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/corgi-cardigan/n02113186_11073.jpg")}>更新</button>
+      {/* <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/corgi-cardigan/n02113186_11073.jpg")}>更新</button> */}
+      <button onClick={handleClick}>更新</button>
     </header>
   )
 }
