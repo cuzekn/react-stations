3d401e73692f8aab8aa09fc28b74d4fb8af5f424
cuzekn <86640029+cuzekn@users.noreply.github.com>
2021-10-19 18:18:39 +0900

    updateImage

--- src/App.js
+++ src/App.js
@@ -1,42 +1,51 @@
 // DO NOT DELETE
 
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react'
 import * as react from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_3534.jpg');
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_3534.jpg',
+  )
 
-  const ImageUpdata = () => {
-  fetch('https://dog.ceo/api/breeds/image/random')
-    .then(res => res.json())
-    .then(result => {
-      const dog = result.message
-      setDogUrl(dog)
-    })
-    .catch(error => {console.log(error)})
+  const updateImage = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json()) //json化
+      .then(result => (
+        setDogUrl(result.message) //jsonのmessageを表示
+      ))
+      //json形式に出来ない時
+      .catch(error => {
+        console.log('失敗', error)
+      })
   }
   return (
-  <>
-    <header>
-      <h1>Dog Photo</h1>
-    </header>
-    <main>
-      <div className="description">
-        <p>犬の画像を表示するサイトです。</p>
-      </div>
-    <div className="image">
-    <img src={dogUrl} />
-    {/* <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/corgi-cardigan/n02113186_11073.jpg")}>更新</button> */}
-    <button onClick={ImageUpdata}>更新</button>
-    </div>
-    </main>
-  </>
+    <>
+      <header>
+        <h1>Dog Photo</h1>
+      </header>
+      <main>
+        <div className="top">
+        <div className="description">
+          <p>犬の画像を表示するサイトです。</p>
+        </div>
+          <div className="photo">
+            <div className="image">
+            <img src={dogUrl} alt={'犬の画像です'} />
+            </div>
+            <div className="btn">
+            <button onClick={updateImage}>更新</button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 // https://dog.ceo/api/breeds/image/random
-// {"message":"https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_2861.jpg","status":"success"}
+// {"message":"https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_2861.jpg","status":"success"} 
